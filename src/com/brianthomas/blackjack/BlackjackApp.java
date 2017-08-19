5bb51748e49394800e7ddc80335769f71499d2e8
Brian Thomas <brianraythomas@gmail.com>
2017-08-18 21:24:01 -0600

    player name added

--- src/com/brianthomas/blackjack/BlackjackApp.java
+++ src/com/brianthomas/blackjack/BlackjackApp.java
@@ -1,20 +1,38 @@
 package com.brianthomas.blackjack;
 
+import java.util.Scanner;
+
 public class BlackjackApp {
 	public static void main(String[] args) {
 
+		BlackjackApp bj = new BlackjackApp();
+		bj.setUpGame();
+		
+	}
+
+	private void setUpGame() {
+		// create a deck and shuffle it
 		Deck deck = new Deck();
 		deck.buildDeck();
 		deck.shuffleDeck();
-
+		
+		// Give both the player and dealer hands
+		Player player = new Player();
 		Hand playerHand = new Hand();
+		player.setHand(playerHand);
+		
 		Hand dealerHand = new Hand();
-
+		
+		namePlayer(player);
+		System.out.println("Howdy, " + player.getName());
+		
+		// Deal two cards to player and dealer
 		playerHand.addCard(deck.dealCard());
 		dealerHand.addCard(deck.dealCard());
+		
 		playerHand.addCard(deck.dealCard());
 		dealerHand.addCard(deck.dealCard());
-
+		
 		System.out.println(playerHand.getCardsInHand());
 		System.out.println(playerHand.getValueOfHand());
 		
@@ -22,9 +40,18 @@ public class BlackjackApp {
 		
 		System.out.println(dealerHand.getCardsInHand());
 		System.out.println(dealerHand.getValueOfHand());
-
+		
 		// for (Card card : deck.getDeck()) {
 		// System.out.println(card);
 		// }
+		
+	}
+
+	private void namePlayer(Player player) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("What's your name?");
+		System.out.print(">> ");
+		player.setName(input.nextLine());
+		input.close();
 	}
 }
--- src/com/brianthomas/blackjack/Player.java
+++ src/com/brianthomas/blackjack/Player.java
@@ -5,6 +5,37 @@ public class Player {
 	private String name;
 	private double wallet;
 	
+	
+	public Hand getHand() {
+		return hand;
+	}
+
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getWallet() {
+		return wallet;
+	}
+
+
+	public void setWallet(double wallet) {
+		this.wallet = wallet;
+	}
+
+
 	public void placeWager(int wager) {
 		
 	}
