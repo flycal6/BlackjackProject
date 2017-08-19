0ec263177331acfb9579baa13c2750baa199f7ed
Brian Thomas <brianraythomas@gmail.com>
2017-08-18 21:38:42 -0600

    card Rank, Suit, and toString tests pass

--- tests/com/brianthomas/blackjack/BlackjackTests.java
+++ tests/com/brianthomas/blackjack/BlackjackTests.java
@@ -25,5 +25,29 @@ public class BlackjackTests {
 		String string = twoSpades.toString();
 		assertEquals("two of spades", string );
 	}
+	
+	@Test
+	public void test_Card_Suit() {
+		Card club = new Card(Suit.CLUBS, Rank.ACE);
+		Card spade = new Card(Suit.SPADES, Rank.EIGHT);
+		Card diamond = new Card(Suit.DIAMONDS, Rank.FIVE);
+		Card heart = new Card(Suit.HEARTS, Rank.JACK);
+		assertEquals(Suit.CLUBS, club.getSuit());
+		assertEquals(Suit.SPADES, spade.getSuit());
+		assertEquals(Suit.DIAMONDS, diamond.getSuit());
+		assertEquals(Suit.HEARTS, heart.getSuit());
+	}
+	
+	@Test
+	public void test_Card_Rank() {
+		Card club = new Card(Suit.CLUBS, Rank.ACE);
+		Card spade = new Card(Suit.SPADES, Rank.EIGHT);
+		Card diamond = new Card(Suit.DIAMONDS, Rank.FIVE);
+		Card heart = new Card(Suit.HEARTS, Rank.JACK);
+		assertEquals(Rank.ACE, club.getRank());
+		assertEquals(Rank.EIGHT, spade.getRank());
+		assertEquals(Rank.FIVE, diamond.getRank());
+		assertEquals(Rank.JACK, heart.getRank());
+	}
 
 }
