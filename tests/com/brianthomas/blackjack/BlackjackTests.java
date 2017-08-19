92032939f379a2d7162b803a6e24582e9b0b3b2a
Brian Thomas <brianraythomas@gmail.com>
2017-08-18 21:23:42 -0600

    first test implemented

--- tests/com/brianthomas/blackjack/BlackjackTests.java
+++ tests/com/brianthomas/blackjack/BlackjackTests.java
@@ -7,18 +7,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BlackjackTests {
+	BlackjackApp bj;
 
 	@Before
 	public void setUp() throws Exception {
+		bj = new BlackjackApp();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		bj = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_Card_object_has_expected_Rank_and_Suit_toString() {
+		Card twoSpades = new Card(Suit.SPADES, Rank.TWO);
+		String string = twoSpades.toString();
+		assertEquals("two of spades", string );
 	}
 
 }
