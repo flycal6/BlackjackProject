d58c780f34e78406d8ad6f72aed11b1261b06c45
Brian Thomas <brianraythomas@gmail.com>
2017-08-20 19:53:21 -0600

    changed rank back to only one value, ace value == 1

--- src/com/brianthomas/blackjack/Rank.java
+++ src/com/brianthomas/blackjack/Rank.java
@@ -1,11 +1,11 @@
 package com.brianthomas.blackjack;
 
 public enum Rank {
-	TWO(2, 2), THREE(3, 3), FOUR(4, 4), FIVE(5, 5), SIX(6, 6), SEVEN(7, 7), EIGHT(8, 8), NINE(9, 9), 
-	TEN(10, 10), JACK(10, 10), QUEEN(10, 10), KING(10, 10), ACE(1, 11);
+	TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), 
+	TEN(10), JACK(10), QUEEN(10), KING(10), ACE(1);
 	public int value, highValue;
 
-	private Rank(int lowValue, int highValue) {
-		this.value = highValue;
+	private Rank(int value) {
+		this.value = value;
 	}
 }
