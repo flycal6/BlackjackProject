b7d43927f3d793ef112f49361e4eb5fadc924095
Brian Thomas <brianraythomas@gmail.com>
2017-08-20 21:50:46 -0600

    dealer hole card hidden initially

--- src/com/brianthomas/blackjack/BlackjackApp.java
+++ src/com/brianthomas/blackjack/BlackjackApp.java
@@ -29,6 +29,7 @@ public class BlackjackApp {
 
 		dealCards();
 
+		getValueOfHands();
 		// for (Card card : deck.getDeck()) {
 		// System.out.println(card);
 		// }
@@ -36,7 +37,9 @@ public class BlackjackApp {
 	}
 
 	private void gameLoop() {
-		getValueOfHands();
+
+		dealerHand.getValueOfHand();
+		player.getHand().getValueOfHand();
 		checkForBlackJack();
 		do {
 			askForHit(playerHand);
@@ -47,35 +50,38 @@ public class BlackjackApp {
 
 	private void dealerTurn() {
 		while (dealerHand.getValueOfHand() < 17 && game == "continue") {
-			
-				System.out.println("Dealer Hits");
-				try {
-					Thread.sleep(400);
-				} catch (InterruptedException ie) {
-					ie.printStackTrace();
-				}
-				
-				dealerHand.addCard(deck.dealCard());
-				getValueOfHands();
-				if(dealerHand.getValueOfHand() > 21) {
-					System.out.println("Dealer BUSTS!!!");
-					game = "playerWin";
-					break;
-				}
-			
+
+			System.out.println(dealerHand.getValueOfHand());
+			System.out.println("Dealer Hits");
+			try {
+				Thread.sleep(400);
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+			dealerHand.addCard(deck.dealCard());
+			showDealerHand();
+			if (dealerHand.getValueOfHand() > 21) {
+				System.out.println("Dealer BUSTS!!!");
+				game = "playerWin";
+				break;
+			}
+
 		}
 		checkForWinner();
 	}
 
 	private void checkForWinner() {
-		if ((player.getHand().getValueOfHand() < 22) && (player.getHand().getValueOfHand() > dealerHand.getValueOfHand()) || (dealerHand.getValueOfHand() > 21)) {
+		if ((player.getHand().getValueOfHand() < 22)
+				&& (player.getHand().getValueOfHand() > dealerHand.getValueOfHand())
+				|| (dealerHand.getValueOfHand() > 21)) {
 			System.out.println(player.getName() + " Wins!");
-		} else if(dealerHand.getValueOfHand() > player.getHand().getValueOfHand() && dealerHand.getValueOfHand() < 22) {
+		} else if (dealerHand.getValueOfHand() > player.getHand().getValueOfHand()
+				&& dealerHand.getValueOfHand() < 22) {
 			System.out.println("Dealer Wins!");
-		} else if( player.getHand().getValueOfHand() == dealerHand.getValueOfHand()) {
+		} else if (player.getHand().getValueOfHand() == dealerHand.getValueOfHand()) {
 			System.out.println("PUSH");
 		}
-		
+
 	}
 
 	private void askForHit(Hand hand) {
@@ -110,13 +116,25 @@ public class BlackjackApp {
 
 	}
 
-	private void getValueOfHands() {
+	private void showDealerHand() {
 		System.out.println("Dealer has:");
-		System.out.println(dealerHand.getCardsInHand());
+		for (Card card : dealerHand.getCardsInHand()) {
+			System.out.println(card);
+		}
 		System.out.println(dealerHand.getValueOfHand());
+	}
 
+	private void getValueOfHands() {
+		System.out.println("Dealer is showing:");
+		// don't show the first dealer card
+		for (int i = 0; i < dealerHand.getCardsInHand().size() - 1; i++)
+			System.out.println(dealerHand.getCardsInHand().get(i + 1));
+
+		System.out.println("*********************");
 		System.out.println("\n" + player.getName() + " has:");
-		System.out.println(playerHand.getCardsInHand());
+		for (Card card : playerHand.getCardsInHand()) {
+			System.out.println(card);
+		}
 		System.out.println(playerHand.getValueOfHand());
 
 	}
@@ -149,12 +167,12 @@ public class BlackjackApp {
 	}
 
 	private void namePlayer(Player player) {
-//		System.out.println("What's your name?");
-//		System.out.print(">> ");
-//
-//		player.setName(input.nextLine());
-//
-//		System.out.println("Howdy, " + player.getName());
+		// System.out.println("What's your name?");
+		// System.out.print(">> ");
+		//
+		// player.setName(input.nextLine());
+		//
+		// System.out.println("Howdy, " + player.getName());
 		player.setName("Player");
 	}
 }
